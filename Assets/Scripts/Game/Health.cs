c49ab37de9418a851a7b0a3b2a416c90901e7462
ozn1911 <117126115+ozn1911@users.noreply.github.com>
2023-04-01 03:45:51 +0300

    small change

---
+++ Assets/Scripts/Game/Health.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Game
+{
+    public class Health : MonoBehaviour
+    {
+        [SerializeField]
+        float _baseMaxHP = 100;
+        float _maxHealthPoints;
+        float _healthPoints;
+        [SerializeField, Min(0)]
+        float _shieldPoints = 0;
+        [SerializeField]
+        float _DamageResist = 0;
+
+        private void Awake()
+        {
+            _maxHealthPoints = _baseMaxHP;
+            _healthPoints = _maxHealthPoints;
+        }
+
+        private void Update()
+        {
+            if(_healthPoints <= 0)
+            {
+                SendMessage("CharacterDeath",SendMessageOptions.RequireReceiver);
+            }
+        }
+
+        public void TakeDamage(float Damage)
+        {
+            Damage -= _DamageResist;
+            float DamageBeforeShield = Damage;
+            Damage -= _shieldPoints;
+            if(Damage > 0)
+            {
+                _shieldPoints = 0;
+                _healthPoints -= Damage;
+            }
+            else
+            {
+                _shieldPoints -= DamageBeforeShield;
+            }
+        }
+
+        public void HealDamage(float Heal)
+        {
+            _healthPoints += Heal;
+            if(_healthPoints > _maxHealthPoints)
+            {
+                _healthPoints = _maxHealthPoints;
+            }
+        }
+        
+    }
+}
